fd7044369813863f9bbcc384aed550966c21a81b
Muskan Choudhary <81254978+muskanchoudhary001@users.noreply.github.com>
2025-12-18 20:20:18 +0530

    basic frontend setup

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -4,14 +4,25 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-6 py-4">
-      <Link to="/" className="text-2xl font-bold text-purple-300">
-        Bookly
+
+      {/* Logo + Brand */}
+      <Link to="/" className="flex items-center gap-2">
+        <img
+          src="/src/assets/logo.webp"
+          alt="Bookly Logo"
+          className="w-8 h-8 object-contain"
+        />
+        <span className="text-2xl font-bold text-purple-300">
+          Bookly
+        </span>
       </Link>
 
-      <div className="flex gap-6 text-lg">
+      {/* Navigation Links */}
+      <div className="flex gap-6 text-lg font-bold text-purple-300">
         <Link to="/">Home</Link>
         <Link to="/books/create">Add Book</Link>
       </div>
+
     </nav>
   );
 };
